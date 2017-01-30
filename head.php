1ff40e900a4164ecbe033ccf7a4adcbd31e1329b
OVOlha <oslavuta@gmail.com>
2017-01-30 18:27:18 +0200

    Add files via upload

---
+++ head.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="doc" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html"
+      xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+    <title>Document</title>
+    <link rel='stylesheet' href='css/bootstrap.min.css' type='text/css' media='all'>
+    <link rel='stylesheet' href='css/bootstrap.css' type='text/css' media='all'>
+    <link href="css/style.css" type="text/css" rel="stylesheet"/>
+
+
+    <script src="jquery/jquery-1.12.4.min.js" type="text/javascript"></script>
+    <script src="jquery/jquery-1.12.4.js" type="text/javascript"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/tether.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+
+</head>
+<body>
+<section id="main">
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 logo ">
+            <img src="images/logo.png" alt="shop_logo">
+        </div>
+        <div  id="parent" class="col-md-3 site_name text-center">
+           <div id="child"> <h1>LIGHTSELL</h1></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 menu ">
+            <h3>Menu</h3>
+        </div>
+        <div class="col-md-4 payanddel">
+            <h3>Payment and delievery</h3>
+        </div>
+        <div class="col-md-4 basket text-right" >
+            <h3>Basket</h3>
+        </div>
+    </div>
+    <!--Carousel -->
+<div class="row">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+                <img class="d-block img-fluid" src="images/slide1.png" alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="images/slide2.png" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block img-fluid" src="images/slide1.png" alt="Third slide">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+</section>
+
+<section id="items">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+            <div class="container-fluid">
+                <img src="images/block1.png">
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+</body>
+</html>
